d8fc06b2653eef82aa90cc821d7bfdec74a7bac2
AbdulRehmankm <168673922+AbdulRehmankm@users.noreply.github.com>
2024-12-06 23:36:25 -0800

    Update index.js
    add 2 origans

--- index.js
+++ index.js
@@ -5,25 +5,46 @@ import dotenv from 'dotenv';
 import adminRoutes from './routes/admin.routes.js';
 import itemRoutes from './routes/item.routes.js';
 import orderRoutes from './routes/order.routes.js';
+
 dotenv.config();
 
 const app = express();
 
+// Allowed origins
+const allowedOrigins = [
+  'https://snapmoviehd.com',
+  'http://localhost:3000'
+];
+
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'https://snapmoviehd.com', credentials: true })); // Allow requests from frontend
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Block the request
+      }
+    },
+    credentials: true
+  })
+);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log('Error connecting to MongoDB:', error));
 
-
+// Routes
 app.use('/api/admin', adminRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/orders', orderRoutes);
 
+// Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
