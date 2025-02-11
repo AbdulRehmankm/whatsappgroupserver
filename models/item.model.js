77a84712dcdffdfdc2664dffb4e02bc5c2c19330
AbdulRehmankm <168673922+AbdulRehmankm@users.noreply.github.com>
2025-02-11 11:35:04 -0800

    Update item.model.js

--- models/item.model.js
+++ models/item.model.js
@@ -17,9 +17,9 @@ const itemSchema = new mongoose.Schema({
     },
     popularity: { type: Number, default: 0 },
     isAdmin: { type: Boolean, default: false },
-   
+       // category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
     status: { type: String, enum: ['Pending', 'Approved', 'Canceled'], default: 'Pending' },
-     image1: { type: String }, // URL for image 1
+     // image1: { type: String }, // URL for image 1
 }, { timestamps: true });
 
 const Item = mongoose.model('Item', itemSchema);
